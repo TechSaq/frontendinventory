70149767982d5830d5bd1941d3f0646253ddf110
Mohd Saquib <ans2sqb@gmail.com>
2020-08-04 16:50:35 +0530

    navbar using props

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,10 +3,19 @@ import './App.css';
 import Card from '../Card';
 import Navbar from '../Navbar';
 
+import data from '../data/apireturn.json'
+
 function App() {
+    
+    const categories = data[1]['categories']
+    const subCategories = data[2]['sub-categories']
+
+    console.log(subCategories);
+
     return (
         <div className="App">
-            <Navbar></Navbar>
+            <Navbar categories={categories} sub-categories={subCategories}>
+            </Navbar>
             <div>
                 <div className="main-category">
                     Deployment
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -6,17 +6,20 @@ class Navbar extends React.Component {
 
     componentDidMount() {
         handleMenu();
-        handleDropdown();
+        handleDropdown();        
     }
 
+
     render() {
+        const categories = this.props.categories;
+        const subCategories = this.props['sub-categories'];
         return (
             <div className="navbar">
                 <div className="dropdownbg hide">
                     <span className="dropdown-arrow"></span>
                 </div>
                 <Brand></Brand>
-                <NavMenuList></NavMenuList>
+                <NavMenuList categories={categories} sub-categories={subCategories}></NavMenuList>
             </div>
         )
     }
@@ -31,35 +34,41 @@ function Brand() {
     )
 }
 
-function NavMenuList() {
+function NavMenuList(props) {
+
+    const keysSubCategories = Object.keys(props['sub-categories']);
+
+    const listSubCategories = props['sub-categories'];
+
+    function list(params) {
+        const html = listSubCategories[params].map((item, index) => {
+            const list =
+                <li key={index}>
+                    <a href="#">{item}</a>
+                </li>
+            return list
+        })
+        return html
+    }
+
+    const listDropdownMap = keysSubCategories.map((navItem, index) => {
+        const html =
+            <li className="menu-item" key={index}>
+                <a href="#">{navItem}</a>
+                <ul className="dropdown hide">
+                    {list(navItem)}
+                </ul>
+            </li>
+
+        return html;
+    });
+
     return (
         <>
             <ul className="menu-list">
-                <li className="menu-item">
-                    <a href="#">Learning</a>
-                    <ul className="dropdown hide">
-                        <li><a href="#">Reading</a></li>
-                        <li><a href="#">Online Courses</a></li>
-                        <li><a href="#">Videos</a></li>
-                    </ul>
-                </li>
-                <li className="menu-item">
-                    <a href="#">Launching</a>
-                    <ul className="dropdown hide">
-                        <li><a href="#">Deployment</a></li>
-                        <li><a href="#">Reporting</a></li>
-                    </ul>
-                </li>
-                <li className="menu-item">
-                    <a href="#">Workflow</a>
-                    <ul className="dropdown hide">
-                        <li><a href="#">Typography</a></li>
-                        <li><a href="#">Colors</a></li>
-                        <li><a href="#">Media</a></li>
-                        <li><a href="#">Frameworks &amp; Libraries</a></li>
-                    </ul>
-                </li>
+                {listDropdownMap}
             </ul>
+            
             <div className="menu menu-icon">
                 <img src="/images/icons/menu-icon.svg" alt=""/>
             </div>
